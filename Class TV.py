426ffb1923e244cae0fe70c345c26fbb274234a8
chrismdating1206@gmail.com <christian.dating12>
2023-05-13 21:26:00 +0800

    first commit

---
+++ Class TV.py
@@ -0,0 +1,7 @@
+# CHRISTIAN M. DATING | BSCPE 1-5
+
+# intro
+input("\n\033[94mPress Any Key to Start...\033[0m")
+print("\033[90m=" *100)
+print("\033[93m WELCOME! \033[0m".center(105))
+print("\033[90m=" *100)

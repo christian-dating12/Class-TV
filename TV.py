39961b3c752cb1b7730058bb2af1bddaddb7149c
chrismdating1206@gmail.com <christian.dating12>
2023-05-16 23:14:34 +0800

    Define channel, volume, status_tv (on/off)

--- TV.py
+++ TV.py
@@ -1,7 +1,12 @@
 # CHRISTIAN M. DATING | BSCPE 1-5
 
-# PSEUDOCODE
+class Television:
+
 # Define self, channel, volume, status_TV.
+    def __init__(self, channel, volume, status_TV):
+        self.get_channel = channel
+        self.get_volume = volume 
+        self.status_TV = status_TV
 # If true, turn on tv
 # If false, turn off tv
 # set new channel between 1-120

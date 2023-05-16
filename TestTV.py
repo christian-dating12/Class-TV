b158d2c1ed76e32b1dbc73d4b19a063190ec4d90
chrismdating1206@gmail.com <christian.dating12>
2023-05-17 00:09:24 +0800

    pseudocodes

--- TestTV.py
+++ TestTV.py
@@ -5,3 +5,11 @@ from TV import Television
 # Two objects
 tv_1 = Television
 tv_2 = Television
+
+# PSEUDOCODES
+# display tv_1 channel and volume, display tv_2 channel and volume
+# Set tv_1 channel and volume level
+# Set tv_2 channel and volume level
+# tv_1 channel_up and channel_down
+# tv_2 channel_up and channel_down
+# tv_1 volume_up and volume_down

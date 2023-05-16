3c1730999850b6cde66d017e923507559c1d009b
chrismdating1206@gmail.com <christian.dating12>
2023-05-17 00:21:46 +0800

    set tv_1 channel and volume level

--- TestTV.py
+++ TestTV.py
@@ -12,11 +12,16 @@ def main():
     tv_1 = Television(30, 3, True)
     tv_2 = Television(3, 2, True)
 
-# display tv_1 channel and volume, display tv_2 channel and volume
+    # display tv_1 channel and volume, display tv_2 channel and volume
     print("tv_1's channel is", tv_1.get_channel, "and volume level is", tv_1.get_volume)
     print("tv_2's channel is", tv_2.get_channel, "and volume level is", tv_2.get_volume)
 
-# tv_1 channel and volume, tv_2 channel and volume
+    # Set tv_1 channel and volume level
+    tv_1.set_channel(18)
+    print("New Channel tv_1:", tv_1.get_channel)
+    tv_1.set_volume(9)
+    print("The new current volume for tv_1 is", tv_1.get_volume,".")
+
 # Set tv_1 channel and volume level
 # Set tv_2 channel and volume level
 # tv_1 channel_up and channel_down

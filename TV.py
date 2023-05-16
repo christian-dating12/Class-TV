b4ac10a10760bc6f30fa94e0428527bf12694139
chrismdating1206@gmail.com <christian.dating12>
2023-05-16 23:07:13 +0800

    pseudocode

---
+++ TV.py
@@ -0,0 +1,12 @@
+# CHRISTIAN M. DATING | BSCPE 1-5
+
+# PSEUDOCODE
+# Define self, channel, volume, status_TV.
+# If true, turn on tv
+# If false, turn off tv
+# set new channel between 1-120
+# set new volume between 1-7
+# Increase the channel number by 1
+# Decrease the channel number by 1
+# Increase the volume level by 1
+# Decrease the volume level by 1
